0403bbdeb6b827146b8543021539b1340b46fac4
AlessandroSestili <sestilialessandro@gmail.com>
2021-06-14 16:15:50 +0200

    creo constructo e stampo

--- index.php
+++ index.php
@@ -7,4 +7,32 @@ class Movie {
     public $nome;
     public $cognome;
     public $eta;
+    public $email;
+
+    // Creo Constructo
+    function __construct($nome , $cognome , $eta , $email) {
+        $this->nome = $nome;
+        $this->cognome = $cognome;
+        $this->eta = $eta;
+        $this->email = $email;
+    }
 }
+
+$paperino = new Movie("Alessandro", "Sestili", 24, "sestilialessandro@gmail.com");
+$topolino = new Movie("Simone", "Casetti", 56, "asd@gmail.com");
+
+
+echo $paperino->nome;
+echo $paperino->cognome;
+echo $paperino->eta;
+echo $paperino->email;
+
+echo $topolino->nome;
+echo $topolino->cognome;
+echo $topolino->eta;
+echo $topolino->email;
+
+
+
+
+
